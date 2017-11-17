4858255206d9b1f39516b330b4b15121f3e9eb83
Giszero <rkdlf_01@nvaer.com>
2017-11-17 22:49:43 +0900

    second commit

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "signal_handlers.h"
+#include "commands.h"
+#include "built_in.h"
+#include "utils.h"
+
+int main()
+{
+  char buf[8096];
+  signal(SIGTSTP,(void *)catch_sigtstp);
+  signal(SIGINT, (void *)catch_sigint);
+  while (1) {
+    fgets(buf, 8096, stdin);
+    signal(SIGINT,(void *)catch_sigint);
+    signal(SIGTSTP,(void *)catch_sigtstp);
+    struct single_command commands[512];
+    int n_commands = 0;
+    mysh_parse_command(buf, &n_commands, &commands);
+
+    int ret = evaluate_command(n_commands, &commands);
+
+    free_commands(n_commands, &commands);
+    n_commands = 0;
+
+    if (ret == 1){
+      break;
+    }
+  }
+
+  return 0;
+}
